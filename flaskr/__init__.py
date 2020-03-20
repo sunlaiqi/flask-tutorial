143b9f92e00920627f8aecbd12fa1f6195d2f382
Michael(Laiqi) Sun <laiqsun@qualcomm.com>
2020-03-20 11:13:27 -0700

    add auth and blog functions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,10 @@ import os
 
 from flask import Flask
 
+from . import db
+from . import auth
+from . import blog
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -28,4 +32,10 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
     
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
